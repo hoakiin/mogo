cb4a9e8a3f0c847d5db310799119c333e3bdd5ba
hoakiin <millychak89@gmail.com>
2025-10-12 20:50:04 +0300

    feat: add service layout and styles

--- src/js/main.js
+++ src/js/main.js
@@ -11,3 +11,23 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.custom-prev',
   },
 });
+
+
+const accordionItems = document.querySelectorAll('.accordion-item');
+
+if (accordionItems.length > 0) {
+  accordionItems[0].classList.add('open');
+}
+
+accordionItems.forEach((item) => {
+  const header = item.querySelector('.accordion-header');
+
+  header.addEventListener('click', (e) => { 
+    if (item.classList.contains('open')) {
+      item.classList.remove('open');
+    } else {
+      accordionItems.forEach((i) => i.classList.remove('open'));
+      item.classList.add('open');
+    }
+  });
+});
